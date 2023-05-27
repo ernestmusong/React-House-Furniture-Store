9603ba4ed90c5a79b3eccb7f93ab37cd09b00f04
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-05-27 23:19:59 +0100

    fix route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import DetailsPage from './components/DetailsPage';
-import Cart from './components/Cart/Cart';
+import Layout from 'components/Layout';
+import Home from 'components/Home';
+import DetailsPage from 'components/DetailsPage';
+import Cart from 'components/Cart/Cart';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ModalPage from './components/ModalPage';
+import ModalPage from 'components/ModalPage';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="/detail/:id" element={<DetailsPage />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/modal/:id" element={<ModalPage />} />
+      <Route path="detail/:id" element={<DetailsPage />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="modal/:id" element={<ModalPage />} />
     </Route>
   </Routes>
 );
